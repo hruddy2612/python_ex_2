957fc5b4ea2d263338f0652fd20ab3f4862b157d
Heather <hruddy26@yahoo.com>
2021-06-08 09:33:39 -0400

    go over and fix all errors

--- ex_2_task_2.py
+++ ex_2_task_2.py
@@ -30,6 +30,65 @@
 
 # import your function from the previous .py file as a module (you can abbreviate it)
 # use ex_2_task_2 here instead once your function works!
+
+def is_valid_email_address(s):
+    s = "hruddy@iastate.edu" 
+
+    is_valid_email_address (s)
+
+    #how many characters in A (characters before the @)
+    A = s.split("@")
+   
+    if len(A) < 3 or len(A) >16: # A needs to be within 3-16 characters, if outside the range, an error occurs
+        print ("error 1")
+    else:
+        print ("ok") #the character amount is ok 
+
+    
+    #how many characters in B (after the @ but before the '.') I don't know how to get B out of the string.
+    B = s.split (".")
+    if len(B) <2 or len(B) >8:
+        print ("error 2")
+    else print ("ok")
+
+    #is C one of the correct endings?
+
+    # I don't know how to check if the email ends in comm, edu, org, gov
+    # I also do not understand how return function works. I have looked through lecture notes/videos and
+    #I have watched additional youtube videos and I don't understand it. I will try to set up a meeting with you Monday. 
+    #I also looked through your solutions and I don't follow them. 
+
+    for each email: #This is not correct code, but I am not sure how to write this
+        if there are no errors
+        print ("ok, seems legit")
+        else
+        print ("error code, with error code message")
+    
+
+# This if ensures that the following is NOT run if this file was imported as a module (which we'll do next!)
+if __name__ == "__main__":
+
+    # tests, including edge cases (incomplete? add more!)
+    email_list = ["charding@iastate.edu", 
+        "chris.edu",
+        "chris@edu",
+        "@bla.edu",
+        "throatwobblermangrove@mpfc.org", 
+        "chris@X.com",
+        "chris.harding@iastate.edu",
+        "chris@pymart.biz",
+        "chris@letsgo!.org",
+        "chris@megasavings.org",
+        "tc@tank.com",
+        ]
+    # validate each email from the list
+    for e in email_list:
+        r, s = is_valid_email_address(e) 
+        if r == None:
+            print(e, s) # OK
+        else:
+            print(f"{e} - error: {s}, error code: {r}") # Error
+
 from ex_2_task_1_solution import is_valid_email_address as is_valid 
 
 gave_up = False
